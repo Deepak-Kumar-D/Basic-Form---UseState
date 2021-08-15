1690a41382f6d3cfeb3e3ef3901e80ea5321ea15
Deepak Kumar D <deepak.kumard36@gmail.com>
2021-08-16 02:31:57 +0530

    first commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,95 @@
-import logo from './logo.svg';
-import './App.css';
+import { useState } from "react";
+import "./App.css";
 
 function App() {
+  const [user, setUser] = useState({
+    fname: "",
+    lname: "",
+    phone: "",
+    email: "",
+  });
+
+  const onChangeUser = (e) => {
+    setUser({ ...user, [e.target.name]: e.target.value });
+  };
+
+  const onSubmit = (data) => {
+    data.preventDefault();
+    console.log(user);
+  };
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <h1>Basic Form</h1>
       </header>
+
+      <section>
+        <form className="input-form">
+          <>
+            <label htmlFor="fname">First Name</label>
+            <input
+              type="text"
+              name="fname"
+              value={user.fname}
+              placeholder="First Name"
+              onChange={onChangeUser}
+            />
+          </>
+
+          <>
+            <label htmlFor="lname">Last Name</label>
+            <input
+              type="text"
+              name="lname"
+              value={user.lname}
+              placeholder="Last Name"
+              onChange={onChangeUser}
+            />
+          </>
+
+          <>
+            <label htmlFor="phone">Phone</label>
+            <input
+              type="text"
+              name="phone"
+              value={user.phone}
+              placeholder="00-000-00000"
+              onChange={onChangeUser}
+            />
+          </>
+
+          <>
+            <label htmlFor="email">Email-Id</label>
+            <input
+              type="text"
+              name="email"
+              value={user.email}
+              placeholder="Email-Id"
+              onChange={onChangeUser}
+            />
+          </>
+        </form>
+
+        <form className="display-form" onSubmit={onSubmit}>
+          <p>
+            First Name: <span>{user.fname}</span>
+          </p>
+
+          <p>
+            Last Name: <span>{user.lname}</span>
+          </p>
+
+          <p>
+            Phone: <span>{user.phone}</span>
+          </p>
+
+          <p>
+            Email-Id: <span>{user.email}</span>
+          </p>
+
+          <input type="submit" value="SUBMIT" />
+        </form>
+      </section>
     </div>
   );
 }
